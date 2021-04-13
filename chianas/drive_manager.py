3d5b9d15f4aaf740d926004f6ec0fd8c5550b508
rjsears <richardjsears@gmail.com>
2021-04-13 09:51:02 -0700

    v0.4 update - see readme for changes

--- chianas/drive_manager.py
+++ chianas/drive_manager.py
@@ -3,7 +3,7 @@
 # -*- coding: utf-8 -*-
 
 __author__ = 'Richard J. Sears'
-VERSION = "0.3 (2021-04-04)"
+VERSION = "0.4 (2021-04-13)"
 
 ### Simple python script that helps to move my chia plots from my plotter to
 ### my nas. I wanted to use netcat as it was much faster on my 10GBe link than
@@ -13,6 +13,17 @@ VERSION = "0.3 (2021-04-04)"
 
 
 # Updates
+#   V0.4 2021-04-13
+# - Added ability to "offline" a drive for maintenance. Before, the script would
+#   select the very first available drive (based on drive number: drive0, drive1)
+#   for plot storage. I ran into a problem when one of my drives kicked out a
+#   smartctl error and I needed to move the plots off of it before it failed. As
+#   soon as I started to move them, drive_manager.py started to fill the drive
+#   back up. So now you can offline and drive_manager will not use it until
+#   you online it again. You still need to go into your chia harvester config
+#   and remove the drive from there.
+#
+#
 #   V0.3 2021-04-04
 # - Added multiple command line utilities to drive_manager.py including:
 #        * -dr or --drive_report    Immediately runs the Daily Report and sends email (if configured)
@@ -42,7 +53,7 @@ sys.path.append('/root/plot_manager')
 import subprocess
 import shutil
 import psutil
-from pySMART import Device  # CAUTION - DO NOT use PyPI version, use https://github.com/truenas/py-SMART
+from pySMART import Device, DeviceList # CAUTION - DO NOT use PyPI version, use https://github.com/truenas/py-SMART
 from psutil._common import bytes2human
 import logging
 from system_logging import setup_logging
@@ -59,6 +70,7 @@ import time
 config = configparser.ConfigParser()
 import argparse
 import textwrap
+from natsort import natsorted
 
 # Define some colors for our help message
 red='\033[0;31m'
@@ -68,10 +80,12 @@ white='\033[0;37m'
 blue='\033[0;34m'
 nc='\033[0m'
 
+# Remove if not using Sentry.io
+# also remove capture(e) in functions below.
 import sentry_sdk
 
 sentry_sdk.init(
-    "https://ksjdfhkjsdhfkjhkfshkjdhfkjsfhkhfk.ingest.sentry.io/ffffdsdfsd",
+    "https://kjhsflkjshdflkjhdslkfjhaslkdjfhlakjsd.ingest.sentry.io/98793879",
 
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
@@ -85,6 +99,7 @@ nas_server = 'chianas01'
 plot_size_k = 108995911228
 plot_size_g = 101.3623551
 receive_script = '/root/plot_manager/receive_plot.sh'
+
 today = datetime.today().strftime('%A').lower()
 current_military_time = datetime.now().strftime('%H:%M:%S')
 current_timestamp = int(time.time())
@@ -130,20 +145,46 @@ program_descripton = f'''
                                 over the past 24 hours. Use with {nc}CAUTION!{blue}. This {nc}should{blue} be ran
                                 from crontab once every 24 hours only! It updates the total
                                 from the last time is was run until now, hence why you should
-                                only run this once per 24 hours.
+                                only run this once per 24 hours.{nc}
+    
+    {green}-off {nc}or{green} --offline_hdd{blue}       This takes a drive as it's input (for example {yellow} drive6{blue}) and
+                                "{red}offlines{blue}" it so that no more plots will get written to it. 
+                                You must {green}--on{blue} or {green}--online_hdd{blue} the drive for it to be used
+                                again. Useful if the drive is failing and needs to be replaced.
+                                You cannot "{red}offline{blue} a drive that is not mounted.
+    
+    {green}-on {nc}or{green} --online_hdd{blue}         This takes a drive as it's input (for example {yellow} drive6{blue}) and
+                                "{green}onlines{blue}" it so that plots will get written to it. This option
+                                will be {nc}UNAVAILABLE{blue} if there are no drives that have been 
+                                offlined!{nc}
+                                
 
     USAGE:
     '''
 
 # Grab command line arguments if there are any
 def init_argparser():
+    with open('offlined_drives', 'r') as offlined_drives_list:
+        offlined_drives = [current_drives.rstrip() for current_drives in offlined_drives_list.readlines()]
     parser = argparse.ArgumentParser(description=program_descripton, formatter_class=RawFormatter)
     parser.add_argument('-v', '--version', action='version', version=f'{parser.prog} {VERSION}')
     parser.add_argument('-dr', '--daily_report', action='store_true', help='Run the ChiaPlot Daily Email Report and exit')
     parser.add_argument('-ct', '--check_temps', action='store_true', help='Return a list of drives and their temperatures and exit')
     parser.add_argument('-pr', '--plot_report', action='store_true', help='Return the total # of plots on the system and total you can add and exit')
     parser.add_argument('-ud', '--update_daily', action='store_true', help=f'Updates 24 hour plot count. {red}USE WITH CAUTION, USE WITH CRONTAB{nc}')
+    parser.add_argument('-off', '--offline_hdd', action='store', help=f'Offline a specific drive. Use drive number: {green}drive6{nc}')
+    if offlined_drives != []:
+        parser.add_argument('-on', '--online_hdd', action='store', help=f'Online a specific drive.' , choices=offlined_drives)
     return parser
+
+
+def get_offlined_drives():
+    with open('offlined_drives', 'r') as offlined_drives_list:
+        offlined_drives = [current_drives.rstrip() for current_drives in offlined_drives_list.readlines()]
+        if offlined_drives != None:
+            return offlined_drives
+        else:
+            return False
 
 
 # Setup to read and write to our config file.
@@ -209,6 +250,18 @@ def get_drive_info(action, drive):
     if action == 'total_current_plots_by_mountpoint':
         return int(bytesto(shutil.disk_usage(drive)[1], 'g') / plot_size_g)
 
+
+
+def dev_test(drive):
+    return shutil.disk_usage(drive)
+    #return Device(drive)
+
+def get_drive_by_mountpoint(mountpoint):
+    """
+    This accepts a mountpoint ('/mnt/enclosure0/rear/column2/drive32') and returns the drive:
+    drive32
+    """
+    return (mountpoint.split("/")[5])
 
 def get_mountpoint_by_drive_number(drive):
     """
@@ -296,6 +349,7 @@ def get_plot_drive_with_available_space():
     looks for any drive that has enough space for at least one plot (k32), sorts
     that list based on the /dev/sdx sorting and then returns the mountpoint and
     the device of each drive.
+    ======> Currently Unused <======
     """
     available_drives = []
     for part in psutil.disk_partitions(all=False):
@@ -307,21 +361,26 @@ def get_plot_drive_with_available_space():
 
 def get_plot_drive_to_use():
     """
-    This looks at all available plot drives that start with /dev/sd and include
-    /mnt/enclosure in the mount path (this covers all of my plot drives), it then
-    looks for any drive that has enough space for at least one plot (k32), sorts
-    that list based on the /dev/sdx sorting and then returns the mountpoint of the
-    device we want to use. Basically the same as above but simply returns the 'next'
-    available drive we want to use.
-    #TODO incorporate in get_plot_drive_with_available_space()
-    """
+        This looks at all available plot drives that start with /dev/sd and include
+        /mnt/enclosure in the mount path (this covers all of my plot drives), it then
+        looks for any drive that has enough space for at least one plot (k32), sorts
+        that list based on the drive# sorting (drive0, drive10, etc) sorting and then
+        returns the mountpoint of the device we want to use. Basically the same as above
+        but simply returns the 'next' available drive we want to use. This also checks
+         to make sure the drive selected has not been marked as "offline".
+        #TODO incorporate in get_plot_drive_with_available_space()
+        """
+    with open('offlined_drives', 'r') as offlined_drives_list:
+        offlined_drives = [current_drives.rstrip() for current_drives in offlined_drives_list.readlines()]
     available_drives = []
     for part in psutil.disk_partitions(all=False):
-        if part.device.startswith('/dev/sd') and part.mountpoint.startswith('/mnt/enclosure') and get_drive_info(
-                'space_free_plots_by_mountpoint', part.mountpoint) >= 1:
-            available_drives.append((part.mountpoint, part.device))
-    return (sorted(available_drives, key=lambda x: x[1]))[0][0]
-
+        if part.device.startswith('/dev/sd') \
+                and part.mountpoint.startswith('/mnt/enclosure') \
+                and get_drive_info('space_free_plots_by_mountpoint', part.mountpoint) >= 1 \
+                and get_drive_by_mountpoint(part.mountpoint) not in offlined_drives:
+            drive = get_drive_by_mountpoint(part.mountpoint)
+            available_drives.append((part.mountpoint, part.device, drive))
+    return (natsorted(available_drives)[0][0])
 
 def get_sorted_drive_list():
     """
@@ -330,9 +389,9 @@ def get_sorted_drive_list():
     available_drives = []
     for part in psutil.disk_partitions(all=False):
         if part.device.startswith('/dev/sd') and part.mountpoint.startswith('/mnt/enclosure'):
-            available_drives.append((part.mountpoint, part.device))
-    return (sorted(available_drives, key=lambda x: x[1]))
-
+            drive=get_drive_by_mountpoint(part.mountpoint)
+            available_drives.append((part.mountpoint, part.device, drive))
+    return natsorted(available_drives)
 
 def get_current_plot_drive_info():
     """
@@ -360,6 +419,56 @@ def log_drive_report():
         get_device_by_mountpoint(get_plot_drive_to_use())[0][0]))
 
 
+def online_offline_drive(drive, onoffline):
+    log.debug(f'online_offline_drive() called with [{drive}] , [{onoffline}]')
+    if get_device_info_by_drive_number(drive) == None:
+        print()
+        print(f'{red}WARNING{nc}: {blue}{drive}{nc} does not exist or is not mounted on this system!')
+        print()
+        log.debug(f'Drive: {drive} does not exist or is not mounted on this system!')
+    else:
+        if onoffline == 'offline':
+            offlined_drives = []
+            with open('offlined_drives', 'r') as offlined_drives_list:
+                offlined_drives = [current_drives.rstrip() for current_drives in offlined_drives_list.readlines()]
+                if drive in offlined_drives:
+                    print()
+                    print(f'Drive: {blue}{drive}{nc} Already in {red}OFFLINE{nc} mode! No action taken.')
+                    print()
+                    log.debug(f'Drive: {drive} Already in offline mode!')
+                else:
+                    offlined_drives.append(drive)
+                    with open('offlined_drives', 'w') as offlined_drive_list:
+                        offlined_drive_list.writelines("%s\n"  % drives for drives in offlined_drives)
+                        print()
+                        print(f'Drive: {blue}{drive}{nc} Put into {red}OFFLINE{nc} mode! Plots will not be written to this drive!')
+                        print()
+                        log.debug(f'Drive: {drive} Put into OFFLINE mode! Plots will not be written to this drive!')
+        elif onoffline == 'online':
+            offlined_drives = []
+            with open('offlined_drives', 'r') as offlined_drives_list:
+                offlined_drives = [current_drives.rstrip() for current_drives in offlined_drives_list.readlines()]
+                if drive in offlined_drives:
+                    offlined_drives.remove(drive)
+                    with open('offlined_drives', 'w') as offlined_drive_list:
+                        offlined_drive_list.writelines("%s\n"  % drives for drives in offlined_drives)
+                        print()
+                        print(f'Drive: {blue}{drive}{nc} Put into {green}ONLINE{nc} mode! Plots will now be written to this drive!')
+                        print()
+                        log.debug(f'Drive: {drive} Put into ONLINE mode! Plots will now be written to this drive!')
+                else:
+                    print()
+                    print(f'Drive: {blue}{drive}{nc} was not in {red}OFFLINE{nc} mode! No action taken.')
+                    print()
+                    log.debug(f'Drive: {drive} was not offline!')
+        elif onoffline == 'check':
+            with open('offlined_drives', 'r') as offlined_drives_list:
+                offlined_drives = [current_drives.rstrip() for current_drives in offlined_drives_list.readlines()]
+                if drive in offlined_drives:
+                    return True
+                else:
+                    return False
+
 def update_receive_plot():
     """
     This utilizes the get_plot_drive_to_use() function and builds out
@@ -372,6 +481,8 @@ def update_receive_plot():
     """
 
     log.debug("update_receive_plot() Started")
+    total_serverwide_plots = get_all_available_system_space('used')[1]
+    log.debug(f'Total Serverwide Plots: {total_serverwide_plots}')
     # First determine if there is a remote file transfer in process. If there is, pass until it is done:
     if os.path.isfile(read_config_data('plot_manager_config', 'remote_transfer', 'remote_transfer_active', False)):
         log.debug('Remote Transfer in Progress, will try again soon!')
@@ -457,15 +568,15 @@ def space_report():
     print(f'{blue}############################################################{nc}')
     print(f'{blue}################### {green}{nas_server} Plot Report{blue} ##################{nc}' )
     print(f'{blue}############################################################{nc}')
-    print (f'Total Plots on {green}{nas_server}{nc}:                               {yellow}{get_all_available_system_space("used")[1]}{nc}')
+    print (f'Total Number of Plots on {green}{nas_server}{nc}:                     {yellow}{get_all_available_system_space("used")[1]}{nc}')
     print (f'Total Number of Systemwide Plots Drives:                  {yellow}{get_all_available_system_space("total")[0]}{nc}')
     print (f'Total Number of k32 Plots until full:                   {yellow}{get_all_available_system_space("free")[1]}{nc}')
     print (f'Maximum # of plots when full:                           {yellow}{get_all_available_system_space("total")[1]}{nc}')
     print (f"Plots completed in the last 24 Hours:                     {yellow}{int(read_config_data('plot_manager_config', 'plotting_information', 'current_total_plots_daily', False))}{nc}")
     print (f"Average Plots per Hours:                                 {yellow}{round((int(read_config_data('plot_manager_config', 'plotting_information', 'current_total_plots_daily', False)))/24,1)}{nc}")
-    print (f"Average Plotting Speed Last 24 Hours (TiB/Day):        {yellow}{(int(read_config_data('plot_manager_config', 'plotting_information', 'current_total_plots_daily', False)) * int(plot_size_g)/1000)}{nc} ")
-    print (f"Appx Number of Days to fill all current plot drives:     {yellow} {int(get_all_available_system_space('free')[1] / int(read_config_data('plot_manager_config', 'plotting_information', 'current_total_plots_daily', False)))} {nc} ")
-    print (f"Current Plot Storage Drive:                        {yellow}{(get_device_by_mountpoint(read_config_data('plot_manager_config', 'plotting_drives', 'current_plotting_drive', False))[0][1])}{nc}")
+    print (f"Average Plotting Speed Last 24 Hours (TiB/Day):         {yellow}{round((int(read_config_data('plot_manager_config', 'plotting_information', 'current_total_plots_daily', False)) * int(plot_size_g)/1000),2)}{nc} ")
+    print(f"Appx Number of Days to fill all current plot drives:     {yellow} {int(get_all_available_system_space('free')[1] / int(read_config_data('plot_manager_config', 'plotting_information', 'current_total_plots_daily', False)))} {nc} ")
+    print (f"Current Plot Storage Drive:                       {yellow}{(get_device_by_mountpoint(read_config_data('plot_manager_config', 'plotting_drives', 'current_plotting_drive', False))[0][1])}{nc}")
     print (f"Temperature of Current Plot Drive:                      {yellow}{Device((get_device_by_mountpoint(read_config_data('plot_manager_config', 'plotting_drives', 'current_plotting_drive', False))[0][1])).temperature}°C{nc}")
     print (f"Latest Smart Drive Assessment of Plot Drive:            {yellow}{Device((get_device_by_mountpoint(read_config_data('plot_manager_config', 'plotting_drives', 'current_plotting_drive', False))[0][1])).assessment}{nc}")
     print(f'{blue}############################################################{nc}')
@@ -480,7 +591,7 @@ def temperature_report():
     print(f'{blue}#    {nc}Serial#{blue}     #{nc}     Device{blue}     #{nc}     Drive{blue}     #{nc}    Temp{blue}     #{nc}')
     print(f'{blue}#################################################################{nc}')
     for drive in get_sorted_drive_list():
-        print(f'{blue}#{nc}   {Device(drive[1]).serial}'f'{blue}     #{nc}'f'   {drive[1]}{blue}    #{nc}' f'    {((get_mountpoint_by_device(drive))[0][0][30:])}{blue}    #{nc}' f'     {Device(drive[1]).temperature}°C'f'{blue}     #{nc}')
+        print(f'{blue}#{nc}   {Device(drive[1]).serial}'f'{blue}     #{nc}'f'   {drive[1]}{blue}    #{nc}' f'    {((get_drive_by_mountpoint(drive[0])))}{blue}    #{nc}' f'     {Device(drive[1]).temperature}°C'f'{blue}     #{nc}')
     print(f'{blue}##################################################################{nc}')
     print('')
     print('')
@@ -580,7 +691,6 @@ def send_daily_email():
     send_daily_update_email()
     log.info('Daily Update Email Sent!')
 
-
 def send_new_plot_notification():
     log.debug('send_new_plot_notification() Started')
     if os.path.isfile('new_plot_received'):
@@ -588,7 +698,6 @@ def send_new_plot_notification():
         if read_config_data('plot_manager_config', 'notifications', 'per_plot', True):
             notify('New Plot Received', 'New Plot Received')
         os.remove('new_plot_received')
-
 
 def main():
     parser = init_argparser()
@@ -601,9 +710,17 @@ def main():
         update_daily_plot_counts()
     elif args.check_temps:
         temperature_report()
+    elif args.offline_hdd:
+        online_offline_drive(args.offline_hdd, 'offline')
+    elif get_offlined_drives():
+        if args.online_hdd:
+            online_offline_drive(args.online_hdd, 'online')
+        else:
+            send_new_plot_notification()
+            update_receive_plot()
     else:
-        send_new_plot_notification()
-        update_receive_plot()
+       send_new_plot_notification()
+       update_receive_plot()
 
 
 if __name__ == '__main__':
