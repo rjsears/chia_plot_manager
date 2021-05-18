4e8d5af06a5c25aff28285cccc833c2b206a1016
rjsears <richardjsears@gmail.com>
2021-05-18 12:56:31 -0700

    Update nuke_snap.sh

--- nuke_snap.sh
+++ nuke_snap.sh
@@ -3,22 +3,27 @@
 # Ubuntu likes to install SNAP which I do not use on my servers at all.
 # This script removes snap from the system!
 
-# stop snapd services
-sudo systemctl stop snapd && sudo systemctl disable snapd
-# purge snapd
-sudo apt purge snapd
-# remove no longer needed folders
+# Run me as root......
+if (( $EUID != 0 )); then
+    echo "Please run me as root"
+    exit
+fi
+
+# Stop all snapd services on the server
+systemctl stop snapd && sudo systemctl disable snapd
+# Purge snapd from the server
+apt purge snapd
+# Get rid of unneeded snap folders
 rm -rf ~/snap
-sudo rm -rf /snap /var/snap /var/lib/snapd /var/cache/snapd /usr/lib/snapd
+rm -rf /snap /var/snap /var/lib/snapd /var/cache/snapd /usr/lib/snapd
 
 
 # Make sure nothing reinstalls SNAP again......
-
 cat << EOF > no-snap.pref
 Package: snapd
 Pin: release a=*
 Pin-Priority: -10
 EOF
 
-sudo mv no-snap.pref /etc/apt/preferences.d/
-sudo chown root:root /etc/apt/preferences.d/no-snap.pref
+mv no-snap.pref /etc/apt/preferences.d/
+chown root:root /etc/apt/preferences.d/no-snap.pref
