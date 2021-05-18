87d7160bdc3d7d112f167c5ed4bd9c5600d411c7
rjsears <richardjsears@gmail.com>
2021-05-18 12:37:31 -0700

    Update nuke_snap.sh

--- nuke_snap.sh
+++ nuke_snap.sh
@@ -10,3 +10,15 @@ sudo apt purge snapd
 # remove no longer needed folders
 rm -rf ~/snap
 sudo rm -rf /snap /var/snap /var/lib/snapd /var/cache/snapd /usr/lib/snapd
+
+
+# Make sure nothing reinstalls SNAP again......
+
+cat << EOF > no-snap.pref
+Package: snapd
+Pin: release a=*
+Pin-Priority: -10
+EOF
+
+sudo mv no-snap.pref /etc/apt/preferences.d/
+sudo chown root:root /etc/apt/preferences.d/no-snap.pref
