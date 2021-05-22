c4f3ad338a2058fc92943aeb19886a945777d2ea
rjsears <richardjsears@gmail.com>
2021-05-21 19:02:44 -0700

    Create set_cpu_to_performance.sh

---
+++ extras/set_cpu_to_performance.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+for file in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor; do echo "performance" > $file; done
+cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
