c543085a676a9d6c3c0a09591fba6e7126c1622d
rjsears <richardjsears@gmail.com>
2021-05-25 10:27:03 -0700

    Update set_cpu_to_performance.sh

--- extras/set_cpu_to_performance.sh
+++ extras/set_cpu_to_performance.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
+
+# This should be chmod +x and set in your crontab:
+# @reboot /root/set_cpu_to_performance.sh
+
 cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 for file in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor; do echo "performance" > $file; done
 cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
