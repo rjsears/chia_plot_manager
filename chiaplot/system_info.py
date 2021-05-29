1ea6521b14252245d9462446b04b736de1e83874
rjsears <richardjsears@gmail.com>
2021-05-28 17:44:15 -0700

    added to v0.9

---
+++ chiaplot/system_info.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+system_info.py for use with plot_manager.py
+"""
+
+VERSION = "V0.9 (2021-05-28)"
+
+## Set Notification Accounts#
+alert_email = ['email_one@gmail.com', 'email_two@gmail.com']
+twilio_from = '+10000000'
+twilio_account = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+twilio_token = 'kjshdkjhfkjshlfkjhkajhfkjahsfdkj'
+twilio_to = ['+17763540098', '+17763540086']
+pushbilletAPI = "o.khjsdfiuwyifkjshkjshkajhdiurieuiufqw"  # pushbullet API token (http://www.pushbullet.com)
+
+
+def main():
+    print("This script is not intended to be run directly.")
+    print("This is the systemwide Credentials & Settings module.")
+    print("It is called by other modules.")
+    exit()
+
+
+if __name__ == '__main__':
+    main()
