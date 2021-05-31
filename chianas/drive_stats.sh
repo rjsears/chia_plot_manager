a723d46eb0a4c94dda8bd916ae247f4b19a290c6
rjsears <richardjsears@gmail.com>
2021-05-31 12:17:06 -0700

    Update drive_stats.sh

--- chianas/drive_stats.sh
+++ chianas/drive_stats.sh
@@ -3,4 +3,4 @@
 # Checks to see if 'move_local_plots.py' is running and is so
 # how much drive IO is being used.
 
-/usr/bin/pidstat -G move_local_plots.py -dlhH 5 1 > drive_stats.io
+/usr/bin/pidstat -G move_local_plots.py -dlhH --human 5 1 > drive_stats.io
