ff331d912fdc6b9e68a4b3c76602ebca49067f6f
rjsears <richardjsears@gmail.com>
2023-08-24 15:53:35 -0700

    Update coin_monitor.py

--- coin_monitor/coin_monitor.py
+++ coin_monitor/coin_monitor.py
@@ -3,7 +3,7 @@
 # -*- coding: utf-8 -*-
 
 __author__ = 'Richard J. Sears'
-VERSION = "0.991 (2023-08-22)"
+VERSION = "0.991 (2023-08-24)"
 
 # Simple script to check to see if we have any new Chia coins.
 # This does assume that you installed in '/home/chia/coin_monitor'
@@ -18,10 +18,7 @@ import subprocess
 import logging
 from system_logging import setup_logging
 from system_logging import read_logging_config
-import system_info
-from pushbullet import Pushbullet, errors as pb_errors
-from twilio.rest import Client
-from twilio.base.exceptions import TwilioRestException
+from system_info import new_coin_email, new_coin_sms, from_address
 import configparser
 from jinja2 import Environment, PackageLoader, select_autoescape
 from datetime import datetime
@@ -34,9 +31,11 @@ config = configparser.ConfigParser()
 
 # Are we utilizing pooling plots? If set to True, this will run a plotnft check
 # to see if we have additional chia and claim them if we do. Make sure to set
-# your correct plotnft wallet id.
+# your correct plotnft wallet id as a string, not an int!
 plotnft = True
-plotnft_wallet = 2
+
+# The Plotnft Wallet is actually a string, not an int.
+plotnft_wallet = str(2)
 
 
 # Do some housework
@@ -74,6 +73,7 @@ def update_config_data(file, section, item, value):
     config.write(cfgfile)
     cfgfile.close()
 
+
 # First, let's try and detect how we were installed
 # Depending on how were were installed (APT vs. Git) will
 # depend on how we call chia. If you installed via Git,
@@ -82,6 +82,7 @@ def update_config_data(file, section, item, value):
 # This works for Ubuntu so it may not work for your distro!
 
 def how_installed():
+    log.debug('how_installed() Started')
     cache = apt.Cache()
     cache.open()
     response = "apt"
@@ -95,7 +96,6 @@ def how_installed():
             exit()
     return (response)
 
-
 # First we need to check if we have a plotnft balance available and if we do, we need to go ahead and claim those chia.
 def check_plotnft_balance():
     log.debug('check_plotnft_balance() Started')
@@ -106,28 +106,28 @@ def check_plotnft_balance():
                 check_plotnft_balance_output = subprocess.check_output(['/usr/bin/chia', 'plotnft', 'show'])
             else:
                 check_plotnft_balance_output = subprocess.check_output(['/home/chia/chia-blockchain/venv/bin/chia', 'plotnft', 'show'])
-            plotnft_balance = check_plotnft_balance_output.decode("utf-8")
-            for line in plotnft_balance.split('\n'):
-                parts = line.strip().split(':', 1)
-                if len(parts) == 2 and parts[0] == 'Claimable balance':
-                    value_part = parts[1].split()[0]  # Extract the numerical value part
-                    if float(value_part) > 0:
-                        log.debug(f'You have {value_part} new coins! We will claim them!')
-                        log.critical(f'You have {value_part} new coins! We will claim them!')
+            lines = check_plotnft_balance_output.decode("utf-8").strip().split('\n')
+            for line in lines:
+                if "Claimable balance:" in line:
+                    parts = line.split()
+                    claimable_balance = float(parts[2])
+                    if float(claimable_balance) > 0:
+                        log.debug(f'You have {claimable_balance} new coins! We will claim them!')
+                        log.critical(f'You have {claimable_balance} new coins! We will claim them!')
+                        notify('PlotNFT Coins Found', f'PlotNFT: {claimable_balance} found, attempting to claim!')
                         if how_installed() == 'apt':
                             subprocess.check_output(['/usr/bin/chia', 'plotnft', 'claim', '-i', plotnft_wallet])
                         else:
                             subprocess.check_output(['/home/chia/chia-blockchain/venv/bin/chia', 'plotnft', 'claim', '-i', plotnft_wallet])
-                else:
-                    log.debug('No new Pooling coins found!')
-                    break  # No need to continue processing lines
+                    else:
+                        log.debug('No new Pooling coins found!')
+                        break  # No need to continue processing lines
         except subprocess.CalledProcessError as e:
             print("Error:", e)
     else:
         log.debug('plotnft not configured, skipping!')
 
-
-# Next we need to see if the claim was successful by seeing if we have more coins in out wallet.
+# Next we need to see if the claim was successful by seeing if we have more coins in our wallet.
 # This may actually take a few minutes to show up after we claim them.
 def check_for_chia_coins():
     log.debug('check_for_chia_coins() Started')
@@ -138,9 +138,8 @@ def check_for_chia_coins():
         total_coins = str(get_total_chia_balance())
         log.critical(f'You now have {total_coins} Chia!')
         update_config_data('coin_monitor_config', 'current_coins', 'coins', total_coins)
-        #notify(f"You Now have {read_config_data('coin_monitor_config', 'current_coins', 'coins', False)} Chia Coins",
-        #    f"You Now have {read_config_data('coin_monitor_config', 'current_coins', 'coins', False)} Chia Coins")
-        send_new_coin_email()
+        notify('New Chia Coins', f'You Now have {total_coins} Chia Coins')
+        send_new_coin_email(total_coins)
 
 def get_total_chia_balance():
     log.debug('get_total_chia_balance() Started')
@@ -165,22 +164,18 @@ def extract_total_balance(output):
     else:
         return None
 
-
 # Here we check to see if we are configured to send a "per new coin" email and if so, we send that email.
-def send_new_coin_email():
+def send_new_coin_email(total_coins):
     log.debug('send_new_coin_email() Started')
     if read_config_data('coin_monitor_config', 'notifications', 'per_coin_email', True):
-        for email_address in system_info.new_coin_email:
+        for email_address in new_coin_email:
             send_template_email(template='new_coin.html',
                                 recipient=email_address,
                                 subject='New Chia Coin Received!\nContent-Type: text/html',
                                 current_time=current_military_time,
-                                current_chia_coins=read_config_data('coin_monitor_config', 'current_coins', 'coins', False))
+                                current_chia_coins=total_coins)
     else:
         pass
-
-
-
 
 def send_email(recipient, subject, body):
     """
@@ -192,55 +187,12 @@ def send_email(recipient, subject, body):
     https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-postfix-as-a-send-only-smtp-server-on-ubuntu-20-04
     """
     try:
-        subprocess.run(['mail', '-s', subject, recipient], input=body, encoding='utf-8')
+        subprocess.run(['mail', '-s', subject, '-r', from_address, recipient], input=body, encoding='utf-8')
         log.debug(f"Email Notification Sent: Subject: {subject}, Recipient: {recipient}, Message: {body}")
     except subprocess.CalledProcessError as e:
-        log.debug(f'send_email error: {e}')
+        log.critical(f'send_email error: {e}')
     except Exception as e:
-        log.debug(f'send_email: Unknown Error! Email not sent.')
-
-
-# Setup to send out Pushbullet alerts. Pushbullet config is in system_info.py
-def send_push_notification(title, message):
-    """Part of our notification system. This handles sending PushBullets."""
-    try:
-        pb = Pushbullet(system_info.pushbilletAPI)
-        push = pb.push_note(title, message)
-        log.debug(f"Pushbullet Notification Sent: {title} - {message}")
-    except pb_errors.InvalidKeyError as e:
-        log.debug(f'Pushbullet Exception: Invalid API Key! Message not sent.')
-    except Exception as e:
-        log.debug(f'Pushbullet Exception: Unknown Pushbullet Error: {e}. Message not sent.')
-
-
-
-def send_sms_notification(body, phone_number):
-    """Part of our notification system. This handles sending SMS messages."""
-    try:
-        client = Client(system_info.twilio_account, system_info.twilio_token)
-        message = client.messages.create(to=phone_number, from_=system_info.twilio_from, body=body)
-        log.debug(f"SMS Notification Sent: {body}.")
-    except TwilioRestException as e:
-        log.debug(f'Twilio Exception: {e}. Message not sent.')
-    except Exception as e:
-        log.debug(f'Twilio Exception: {e}. Message not sent.')
-
-
-def notify(title, message):
-    """ Notify system for email, pushbullet and sms (via Twilio)"""
-    log.debug(f'notify() called with Title: {title} and Message: {message}')
-    if (read_config_data('coin_monitor_config', 'notifications', 'alerting', True)):
-        if (read_config_data('coin_monitor_config', 'notifications', 'pb', True)):
-            send_push_notification(title, message)
-        if (read_config_data('coin_monitor_config', 'notifications', 'email', True)):
-            for email_address in system_info.alert_email:
-                send_email(email_address, title, message)
-        if (read_config_data('coin_monitor_config', 'notifications', 'sms', True)):
-            for phone_number in system_info.twilio_to:
-                send_sms_notification(message, phone_number)
-    else:
-        pass
-
+        log.critical(f'send_email: Unknown Error! Email not sent.')
 
 # Thank You - https://frankcorso.dev/email-html-templates-jinja-python.html
 def send_template_email(template, recipient, subject, **kwargs):
@@ -253,11 +205,23 @@ def send_template_email(template, recipient, subject, **kwargs):
     send_email(recipient, subject, template.render(**kwargs))
 
 
+def notify(title, message):
+    """ Notify system for email, and sms (via Email to SMS)"""
+    log.debug(f'notify() called with Title: {title} and Message: {message}')
+    if (read_config_data('farmer_health_config', 'notifications', 'alerting', True)):
+        if (read_config_data('farmer_health_config', 'notifications', 'email', True)):
+            for email_address in new_coin_email:
+                send_email(email_address, title, message)
+        if (read_config_data('farmer_health_config', 'notifications', 'sms', True)):
+            for email_address in new_coin_sms:
+                send_email(email_address, title, message)
+    else:
+        pass
+
+
 def main():
-    how_installed()
     check_plotnft_balance()
     check_for_chia_coins()
-
 
 if __name__ == '__main__':
     main()
